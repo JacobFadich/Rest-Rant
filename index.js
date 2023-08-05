bdc40b796f622110c57c592789a74ad7a5e19935
Jacob Fadich <jacobgamer137@yahoo.com>
2023-08-04 18:37:17 -0700

    Adding home page view

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+  res.render('Home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1> 404 Page</h1>')
+  res.send('404 page')
 })
 
 app.listen(process.env.PORT)
+
