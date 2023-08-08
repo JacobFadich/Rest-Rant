0df076be3584a318e1045c36100d3ef8d6df75dd
Jacob Fadich <jacobgamer137@yahoo.com>
2023-08-08 16:39:00 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,30 +1,19 @@
 const router = require('express').Router()
+const places = require('../Models/places.js')
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+   
+  places.push(req.body)
+  res.redirect('POST /places')
 })
-  
+
 
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/Entre.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/Dessert.jpg'
-      }]
     res.render('places/index', {places})
 })
   
