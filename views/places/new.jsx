6b2296db2cb243c4547b9927e04a1550c6be804e
Jacob Fadich <jacobgamer137@yahoo.com>
2023-08-24 15:21:17 -0700

    part 9 submission

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form () {
+function new_form (data) {
     return (
         <Def>
           <main>
@@ -27,11 +27,17 @@ function new_form () {
                     <label htmlFor="cuisines">Cuisines</label>
                     <input className="form-control" id="cuisines" name="cuisines" required/>
                 </div>
-                <input className="btn btn-primary" type="submit" value="Add Place"/>
-                <div className="form-group">
-                    <label for="founded">Founded Year</label>
-                    <input className="form-control" id="founded" name="founded" />
+                    <input className="btn btn-primary" type="submit" value="Add Place"/>
+                <div className="form-group col-sm-4">
+                    <label htmlFor="founded">Founded Year</label>
+                    <input 
+                    type="number" 
+                    className="form-control" 
+                    id="founded" 
+                    name="founded" 
+                    value={new Date().getFullYear()} />
                 </div>
+
 
             </form>
 
